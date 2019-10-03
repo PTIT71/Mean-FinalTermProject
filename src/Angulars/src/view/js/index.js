9c87d555be9f38e65dda80d1922b4313d5601896
Phan Văn Phước Thịnh <40396579+PTIT71@users.noreply.github.com>
2019-10-04 02:53:16 +0700

    agg

---
+++ src/Angulars/src/view/js/index.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', []);
+
+app.controller('myController', myController);
+
+function myController($scope, $http) {
+    $scope.title = 'Xin chao!!!';
+    $scope.userName = '';
+    $scope.userId = 0;
+
+    $scope.getName = function() {
+        $http.get('/api/getName/1').then(function(result) {
+            $scope.userName = result.data;
+        });
+    }
+    $scope.TenXe = '';
+    $scope.tenxe = 'xeA';
+    $scope.getGiaXe = function() {
+        $http.get('/api/getGiaXe/' + $scope.tenxe).then(function(result) {
+            $scope.TenXe = result.data;
+        });
+    }
+
+
+}
