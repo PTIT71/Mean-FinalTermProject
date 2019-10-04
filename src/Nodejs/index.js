71ff3886a4af2c4dab48bff90f58de9e040f343e
Phan Văn Phước Thịnh <40396579+PTIT71@users.noreply.github.com>
2019-10-04 14:40:41 +0700

    comit fix struture

--- src/Nodejs/index.js
+++ src/Nodejs/index.js
@@ -1,4 +1,3 @@
-
 var path = require('path');
 var express = require('express');
 var serveStatic = require('serve-static');
@@ -24,60 +23,58 @@ const db = require('./db');
 
 //====================================================================
 app.use(serveStatic(path.join(__dirname, 'public')));
-var listUsers = [{id: 1, name: 'Nguyễn Văn A'}, {id: 2, name: 'Hoàng Thị B'}, {id: 3, name: 'Phan Huy C'}];
+var listUsers = [{ id: 1, name: 'Nguyễn Văn A' }, { id: 2, name: 'Hoàng Thị B' }, { id: 3, name: 'Phan Huy C' }];
 
 app.get('/', function(req, res) {
-  res.send('I am listening');
+    res.send('I am listening');
 });
 
 //load 1 trang có sẳn 
-app.get('/home', function(request, response){
-    response.redirect('./src/index.html');
-  });
+app.get('/home', function(request, response) {
+    response.redirect('./src/view/user/index.html');
+});
 
 
-  app.get('/api/getName/:userId', function(request, response){
+app.get('/api/getName/:userId', function(request, response) {
     var userId = request.params.userId;
     var user = listUsers.find(u => u.id == userId)
-    if(user)
-      response.send(user.name);
+    if (user)
+        response.send(user.name);
     else
-      response.send('User not found!!!')
-  });
+        response.send('User not found!!!')
+});
 
-  app.get('/api/getGiaXe/:tenxe', function(req, res){
+app.get('/api/getGiaXe/:tenxe', function(req, res) {
     var ten = req.params.tenxe;
-    var query = {name:ten}
-    db.getDB().collection("XeMay").find(query).toArray((err,result)=>{
-        if(err) throw err;
+    var query = { name: ten }
+    db.getDB().collection("XeMay").find(query).toArray((err, result) => {
+        if (err) throw err;
         else {
             var data = (result[0].cost).toString();
             res.status(200).send(data);
         }
     })
-  });
+});
 
 
-  //load list danh sách sản phẩm
-  app.get('/api/getList/SanPham',function(req,res){
-    db.getDB().collection("SanPham").find().toArray((err,result)=>{
-      if(err) throw err;
-      else {
-        console.log(result);
-        res.send(result);
-      }
+//load list danh sách sản phẩm
+app.get('/api/getList/SanPham', function(req, res) {
+    db.getDB().collection("SanPham").find().toArray((err, result) => {
+        if (err) throw err;
+        else {
+            console.log(result);
+            res.send(result);
+        }
     })
-  })
+})
 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         console.log("Khong ket noi duoc database");
         process.exit();
-    }
-    else{
-        app.listen(3000,()=>{
+    } else {
+        app.listen(3000, () => {
             console.log('connected database , app lissten to 3000');
         })
     }
 })
-
